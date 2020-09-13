cd6f641ffdf47afdc635d2668e5ef3a97d1b188d
Remmo <remmo@remmos-macbook-pro.home>
2020-09-13 14:13:27 +0200

    deployement heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/database");
+const path = require('path')
 
 const app = express();
 
@@ -13,6 +14,21 @@ app.use(express.json({ extended: false }));
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/events", require("./routes/api/events"));
+
+// Serve static assets in production
+app.use(express.static(path.join(__dirname, 'client/build')))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')))
+  //
+  app.get('*', (req, res) => {
+    res.sendfile(path.join((__dirname = 'client/build/index.html')))
+  })
+}
+//build mode
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/public/index.html'))
+})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
